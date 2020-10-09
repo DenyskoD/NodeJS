5d6e59c9e2ee2461a452c7f3ee00d3486bd06435
DenyskoD <deniskodima1@gmail.com>
2020-10-09 20:50:45 +0300

    Create os_ref.js

---
+++ refs/os_ref.js
@@ -0,0 +1,22 @@
+const os = require('os')
+
+// Platform
+console.log(os.platform())
+
+// Architecture
+console.log(os.arch())
+
+// info
+console.log(os.cpus())
+
+// free memory
+console.log(os.freemem())
+
+// total memory
+console.log(os.totalmem())
+
+// main dir
+console.log(os.homedir())
+
+// uptime
+console.log(os.uptime())
